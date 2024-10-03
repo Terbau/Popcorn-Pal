e4d4862886710ebd1c6db0591cc4939d55af2f4c
Terbau <bragebau@hotmail.no>
2024-10-03 12:27:19 +0200

    feat: add MainLayout component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
+import { MainLayout } from "../components/layouts/MainLayout";
+
 export default function HomePage(){
-    return <div>HomePage</div>
+  return (
+    <MainLayout>
+      <h1>Home Page</h1>
+    </MainLayout>
+  )
 }
