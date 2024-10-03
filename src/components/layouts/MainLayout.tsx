def0773523849e400527e35d3cce05bf124eeff5
Terbau <bragebau@hotmail.no>
2024-10-03 12:44:27 +0200

    fix: linting issues

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -7,16 +7,16 @@ interface MainLayoutProps {
 }
 
 export const MainLayout = ({
-	limitWidth = true,
-	children,
+  limitWidth = true,
+  children,
 }: MainLayoutProps) => {
-	return (
-		<div className="w-full bg-primary min-h-screen flex flex-col px-6">
-			{/* Navbar goes here */}
-			<main className={clsx({ "max-w-screen-lg mx-auto my-16": limitWidth })}>
-				{children}
-			</main>
-			{/* Footer goes here */}
-		</div>
-	);
+  return (
+    <div className="w-full bg-primary min-h-screen flex flex-col px-6">
+      {/* Navbar goes here */}
+      <main className={clsx({ "max-w-screen-lg mx-auto my-16": limitWidth })}>
+        {children}
+      </main>
+      {/* Footer goes here */}
+    </div>
+  );
 };
