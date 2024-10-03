2fafa4ceb4f7d2ceee80290590183bb5c2bc1a3a
kaisa.l <kaisa.oyre.larsen@gmail.com>
2024-10-03 12:56:29 +0200

    feat: add navbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from "../components/layouts/MainLayout";
 export default function HomePage(){
   return (
     <MainLayout>
-      <h1>Home Page</h1>
+      <h1 className="text-white text-4xl font-semibold">Home Page</h1>
     </MainLayout>
   )
 }
