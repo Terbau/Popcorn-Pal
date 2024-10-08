262e1b6fe94d7f7f538aa8bcb64d193dd0fe3462
Terbau <bragebau@hotmail.no>
2024-10-08 10:59:49 +0200

    refactor: use apollo instead of redux and tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import ApolloClient, {
+  InMemoryCache,
+  type NormalizedCacheObject,
+} from "apollo-boost";
+import { ApolloProvider as BeforeApolloProvider } from "@apollo/react-hooks";
 import HomePage from "./pages/HomePage";
 
-const queryClient = new QueryClient();
+const cache = new InMemoryCache({});
+
+const client = new ApolloClient<NormalizedCacheObject>({
+  cache: cache,
+  clientState: {
+    defaults: {},
+  },
+});
+
+
+// This is a workaround for typescript screaming at our faces because of apollo boost
+// not configuring types properly
+// biome-ignore lint/suspicious/noExplicitAny: <hopelessly bad typescript support from apollo boost>
+const ApolloProvider = BeforeApolloProvider as any;
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <ApolloProvider client={client}>
       <HashRouter>
         <Routes>
           <Route path="/" Component={HomePage} />
         </Routes>
       </HashRouter>
-    </QueryClientProvider>
+    </ApolloProvider>
   );
 }
 
