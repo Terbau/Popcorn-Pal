91549f7ed047ae5a0a5bae576df39d5ee9b83094
kaisa.l <kaisa.oyre.larsen@gmail.com>
2024-11-01 13:55:38 +0100

    feat: add footer

---
+++ frontend/src/components/molecules/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-brand-3 flex items-center text-white text-center p-4 shadow-lg w-full">
+      <div className="flex items-center space-x-4 font-roboto">
+        <p>hello</p>
+      </div>
+    </footer>
+  );
+};
